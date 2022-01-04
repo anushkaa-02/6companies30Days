9f345f6640679d5e6cc2a3a26cd25463dc938374
Anushka Paul <87390353+pilipi-puu-puu@users.noreply.github.com>
2022-01-04 13:42:06 +0530

    updated code

--- Goldman Sachs/overlapping_rectangles.cpp
+++ Goldman Sachs/overlapping_rectangles.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 class Solution {
   public:
-    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
-        // code here
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) 
+    {
           if(L2[0]>R1[0]||L1[0]>R2[0]||L1[1]<R2[1]||L2[1]<R1[1])
        return 0;
        return 1;
