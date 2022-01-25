d78f411f89d6b766ef0161ddc73284fe1efc6932
Anushka Paul <87390353+pilipi-puu-puu@users.noreply.github.com>
2022-01-25 21:59:43 +0530

    Added programs

---
+++ Intuit/largestnum_k_swaps.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+       void findMaxUtil(string str, string &max, int k, int pos)
+    {
+        if(k == 0)
+            return;
+            
+        char maxm = str[pos];
+        for(int i = pos+1; i < str.length() ;i++)
+        {
+            if(maxm < str[i])
+                maxm = str[i];
+        }
+        
+        if(maxm != str[pos])
+            k--;
+            
+        for(int i=str.length()-1; i>=pos ;i--)
+        {
+            if(str[i] == maxm)
+            {
+                swap(str[i], str[pos]);
+                if(str.compare(max) > 0)
+                    max = str;
+                    
+                findMaxUtil(str, max, k, pos+1);
+                
+                swap(str[i], str[pos]);
+            }
+        }
+    }
+    string findMaximumNum(string str, int k)
+    {
+       string max = str;
+       findMaxUtil(str, max, k, 0);
+       return max;
+       
+    }
+};
+
+int main()
+{
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
+    }
+    return 0;
+}
+
